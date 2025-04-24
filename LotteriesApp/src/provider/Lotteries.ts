aeedf6bbdde504190ae00ec4d7641599cae1ef55
Thiago Souto <thiago.santos@hellofresh.com>
2025-04-24 11:37:19 +0200

    Create Lottetries

--- LotteriesApp/src/provider/Lotteries.ts
+++ LotteriesApp/src/provider/Lotteries.ts
@@ -1,21 +1,28 @@
-export enum HttpMethod {
-    POST = "POST",
-    GET = "GET",
+import { Lottery } from '../types';
+import { logError } from '../utils/logger';
+
+const LOTTERIES_PROVIDER = "LotteriesProvider"
+enum HttpMethod {
+    POST = 'POST',
+    GET = 'GET',
 }
 
+enum Path {
+    LOTTERIES = '/lotteries'
+}
 /**
- * 
+ *
  * @param method support methods, see @type {HttpMethod}
  * @param relativePath relative path to the endpoint
  * @param body body of the request, optional
- * 
+ *
  * @returns @type {Result<T,Error>}
  */
-export async function request<T>(
+async function request<T>(
     method: HttpMethod,
     relativePath: string,
     body?: string | null
-) : Promise<T> {
+): Promise<T> {
     const response = await fetch(`http://localhost:3000${relativePath}`, {
         method: method,
         headers: {
@@ -25,4 +32,19 @@ export async function request<T>(
     });
 
     return await response.json();
+}
+
+/**
+ *
+ * Method accountable to fetch @type {Lottery[]} from api
+ *
+ * @returns @type {Lottery[]}
+ */
+export async function lotteriesListProvider(): Promise<Lottery[]> {
+    try {
+        return await request(HttpMethod.GET, Path.LOTTERIES);
+    } catch (error) {
+        logError(LOTTERIES_PROVIDER, 'failed to fetch lotteries due to exception', error);
+        throw error;
+    }
 }
---
+++ LotteriesApp/src/types.ts
@@ -0,0 +1,9 @@
+export type Status = 'running' | 'finished';
+
+export interface Lottery {
+  id: string;
+  name: string;
+  prize: string;
+  type: string;
+  status: Status;
+}
