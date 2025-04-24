d193a38e62a9c5dcb2bb1c9efb3d3cd0c0689126
Thiago Souto <thiago.santos@hellofresh.com>
2025-04-24 15:43:35 +0200

    Create LotteryCardView

---
+++ LotteriesApp/src/components/LotteryCardView.tsx
@@ -0,0 +1,54 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+import type { Lottery } from '../types';
+
+interface LotteryCardViewProps {
+  lottery: Lottery;
+}
+export const LotteryCardView = ({ lottery }: LotteryCardViewProps) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headline}>{lottery.name}</Text>
+      <Text style={styles.supportingText}>{lottery.prize}</Text>
+      <Text style={styles.supportingText}>{lottery.id}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 8,
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    margin: 8,
+    gap: 5,
+    padding: 16,
+  },
+  icon: {},
+  labelRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headline: {
+    flex: 1,
+    textAlign: 'left',
+    fontSize: 16,
+    lineHeight: 24,
+    fontFamily: 'Roboto',
+    fontWeight: '500',
+    color: '#191C1B',
+  },
+  supportingText: {
+    fontFamily: 'Roboto-Regular',
+    fontWeight: '400',
+    fontSize: 14,
+    lineHeight: 20,
+    letterSpacing: 0.25,
+    color: '#444746',
+  },
+});
--- LotteriesApp/src/pages/LotteriesScreen.tsx
+++ LotteriesApp/src/pages/LotteriesScreen.tsx
@@ -1,19 +1,33 @@
-import { ActivityIndicator, FlatList, Text } from 'react-native';
+import { ActivityIndicator, FlatList, StyleSheet } from 'react-native';
 
+import { LotteryCardView } from '../components/LotteryCardView';
 import { useLotteriesList } from '../hooks/useLotteries';
 
 export const LotteriesScreen = () => {
   const { data, isLoading, isError } = useLotteriesList();
 
   const component = isLoading ? (
-    <ActivityIndicator />
+    <ActivityIndicator style={styles.loading} />
   ) : (
     <FlatList
+      style={styles.cardList}
       data={data}
-      renderItem={({ item }) => <Text>{item.name}</Text>}
+      renderItem={({ item }) => <LotteryCardView lottery={item} />}
       keyExtractor={(item) => item.id}
     />
   );
 
   return component;
 };
+
+const styles = StyleSheet.create({
+  cardList: {
+    marginStart: 16,
+    marginEnd: 16,
+    marginTop: 8,
+  },
+  loading: {
+    justifyContent: 'center',
+    flex: 1,
+  },
+});
