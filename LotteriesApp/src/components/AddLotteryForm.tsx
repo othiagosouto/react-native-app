379268c519f1e81fff11d6a93f1ab1dccbb2825d
Thiago Souto <thiago.santos@hellofresh.com>
2025-04-25 16:30:39 +0200

    Create AddLotteryForm

---
+++ LotteriesApp/src/components/AddLotteryForm.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Text } from 'react-native-paper';
+
+import { ButtonView } from './ButtonView';
+import { TextInputView } from './TextInputView';
+
+interface LotteryFormProps {
+  onSuccess: () => void;
+}
+
+export const AddLotteryForm = ({ onSuccess }: LotteryFormProps) => {
+  const [lotteryName, setLotteryName] = useState('');
+  const [lotteryPrize, setLotteryPrize] = useState('');
+  const [lotteryNameError, setLotteryNameError] = useState<string | undefined>(
+    ''
+  );
+  const [lotteryPrizeError, setLotteryPrizeError] = useState<
+    string | undefined
+  >('');
+  const isValid = lotteryName.length >= 4 && lotteryPrize.length >= 4;
+  const onSubmit = () => {
+    onSuccess();
+  };
+  const handleLotteryNameError = () => {
+    setLotteryNameError(
+      lotteryName.length < 4 ? 'Name must be at least 4 characters' : undefined
+    );
+  };
+  const handleLotteriPrizeError = () => {
+    setLotteryPrizeError(
+      lotteryPrize.length < 4 ? 'You must add a value for prize' : undefined
+    );
+  };
+  return (
+    <View style={styles.mainContainer}>
+      <Text style={styles.title}>Add a new lottery</Text>
+
+      <TextInputView
+        placeholder="Lottery name"
+        error={lotteryNameError}
+        text={lotteryName}
+        setText={setLotteryName}
+        onBlur={handleLotteryNameError}
+      />
+
+      <TextInputView
+        placeholder="Lottery Prize"
+        error={lotteryPrizeError}
+        text={lotteryPrize}
+        setText={setLotteryPrize}
+        onBlur={handleLotteriPrizeError}
+      />
+      <ButtonView disabled={!isValid} label="Add" onClick={onSubmit} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    paddingVertical: 16,
+    paddingHorizontal: 8,
+    justifyContent: 'center',
+    gap: 33,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  }
+});
