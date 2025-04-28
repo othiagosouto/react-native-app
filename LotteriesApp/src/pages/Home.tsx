a6e90bae169189d1468ce5c9045e5506544b3df7
Thiago Souto <thiago.santos@hellofresh.com>
2025-04-28 15:56:48 +0200

    Refactor Home

--- LotteriesApp/src/pages/Home.tsx
+++ LotteriesApp/src/pages/Home.tsx
@@ -1,92 +1,58 @@
-import FontAwesome6 from '@react-native-vector-icons/fontawesome6';
 import { useNavigation } from '@react-navigation/native';
-import {
-  ActivityIndicator,
-  SafeAreaView,
-  FlatList,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { ActivityIndicator } from 'react-native-paper';
 import { useToast } from 'react-native-toast-notifications';
 
 import { Fab } from '../components/Fab';
-import { LotteryCardView } from '../components/LotteryCardView';
+import { LotteriesList } from '../components/LotteriesList';
+import { Searchbar } from '../components/Searchbar';
+import { Title } from '../components/Title';
 import { useLotteriesList } from '../hooks/useLotteries';
 
 export const Home = () => {
+  const [searchText, setSearchText] = useState('');
   const toast = useToast();
   const { data, isLoading, refetch } = useLotteriesList();
   const navigation = useNavigation();
   const onLotteryAdded = () => {
     refetch();
+    toast.show('New lottery added successfully!');
   };
   const handleAddLottery = () => {
     navigation.navigate('AddLottery', { onLotteryAdded: onLotteryAdded });
-    toast.show('New lottery added successfully!');
   };
-
-  const component = isLoading ? (
-    <ActivityIndicator style={styles.loading} />
-  ) : (
-    <FlatList
-      data={data}
-      renderItem={({ item }) => <LotteryCardView lottery={item} />}
-      contentContainerStyle={styles.cardList}
-      keyExtractor={(item) => item.id}
-    />
+  const filteredItems = data?.filter(
+    (item) => item.name.toLowerCase().search(searchText.toLowerCase()) !== -1
   );
 
   return (
-    <SafeAreaView>
-      <View style={styles.screenContainer}>
-        <Title />
-        {component}
-        <Fab action={handleAddLottery} />
-      </View>
+    <SafeAreaView style={styles.container}>
+      <Title text="Lotteries" />
+      <Searchbar onTextChange={setSearchText} style={styles.searchBar} />
+      <LotteriesList
+        items={filteredItems ? filteredItems : []}
+        searchText={searchText}
+      />
+      {isLoading ? <ActivityIndicator style={styles.loading} /> : undefined}
+      <Fab action={handleAddLottery} />
     </SafeAreaView>
   );
 };
 
-const Title = () => {
-  return (
-    <View style={styles.titleContainer}>
-      <Text style={styles.title}>Lotteries</Text>
-      <FontAwesome6
-        name="dice-six"
-        size={36}
-        color="black"
-        iconStyle={'solid'}
-      />
-    </View>
-  );
-};
-
 const styles = StyleSheet.create({
-  screenContainer: {
-    paddingStart: 16,
-    paddingEnd: 16,
-    paddingTop: 32,
-    flex: 1,
+  container: {
     flexDirection: 'column',
+    alignContent: 'center',
+    flex: 1,
   },
-  cardList: {
-    paddingBottom: 110,
+  searchBar: {
+    marginHorizontal: 16,
+    marginBottom: 16,
   },
   loading: {
-    justifyContent: 'center',
-    flex: 1,
-  },
-  title: {
-    fontSize: 25,
-    textAlign: 'center',
-    fontWeight: 'bold',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'baseline',
-    gap: 5,
-    marginBottom: 8,
+    position: 'absolute',
+    start: '50%',
+    top: '50%',
   },
 });
