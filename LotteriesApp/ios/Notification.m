5baa3006cdd1128eeca343bad96620d256a8cbc0
Thiago Souto <thiago.santos@hellofresh.com>
2025-05-05 10:25:10 +0200

    Create method to display notification in IOS

--- LotteriesApp/ios/Notification.m
+++ LotteriesApp/ios/Notification.m
@@ -3,4 +3,6 @@
 #import "React/RCTBridgeModule.h"
 
 @interface RCT_EXTERN_MODULE(Notification, NSObject)
+
+RCT_EXTERN_METHOD(showNotification:(NSString *)title location:(NSString *)body)
 @end
--- LotteriesApp/ios/Notification.swift
+++ LotteriesApp/ios/Notification.swift
@@ -1,9 +1,47 @@
-
 import Foundation
-import UIKit
-import UserNotifications
 
 @objc(Notification)
-class Notification: NSObject {
+class Notification: NSObject, UNUserNotificationCenterDelegate {
+  private let uuid = UUID()
 
+  @objc func showNotification(_ title: String, location body: String) {
+    Task.init { @MainActor in
+
+      do {
+        let isAuthorized = try await requestAuthorizationForNotifications()
+        if isAuthorized {
+          let content = UNMutableNotificationContent()
+          content.title = title
+          content.body = body
+          content.sound = UNNotificationSound.default
+          let center = UNUserNotificationCenter.current()
+
+          let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0.1, repeats: false)
+          let request = UNNotificationRequest(identifier: uuid.uuidString, content: content, trigger: trigger)
+          center.delegate = self
+          try await center.add(request)
+        } else {
+          print("Notification not authorized")
+        }
+      } catch {
+        print(error.localizedDescription)
+      }
+    }
+  }
+
+  func requestAuthorizationForNotifications() async throws -> Bool {
+    let notificationCenter = UNUserNotificationCenter.current()
+    let authorizationOptions: UNAuthorizationOptions = [.alert, .sound, .badge]
+
+    do {
+      let authorizationGranted = try await notificationCenter.requestAuthorization(options: authorizationOptions)
+      return authorizationGranted
+    } catch {
+      throw error
+    }
+  }
+
+  func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    completionHandler([.banner, .sound, .badge])
+  }
 }
